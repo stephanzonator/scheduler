6e9c9b972359606a728e68aafbfb7ec1672edc1b
Stephanie <stephanzonator@hotmail.com>
2021-04-12 16:45:09 -0700

    button component working

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,43 @@
 import React from "react";
+import classNames from 'classnames/bind';
 
 import "components/Button.scss";
 
+// export default function Button(props) {
+//    return <></>;
+// }
+
+
+// export default function Button(props) {
+//    return <button>{props.children}</button>;
+//  }
+
+// let buttonClass = "button";
+
+// if (props.confirm) {
+//   buttonClass += " button--confirm";
+// }
+
+// if (props.danger) {
+//   buttonClass += " button--danger";
+// }
+
 export default function Button(props) {
-   return <></>;
-}
+
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
+ }
+ 
+ 
--- stories/index.js
+++ stories/index.js
@@ -22,3 +22,6 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+
+  
+  
