8b87c8c5da160f6b28b589c3cdd0ab3f318ac207
Stephanie <stephanzonator@hotmail.com>
2021-04-12 17:40:23 -0700

    styled buttons, dayListItem text formatted

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,28 @@
 import React from "react";
+import classNames from 'classnames/bind';
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots
+  });
+
+  let spotNum = props.spots;
+  let spotPlural = "spots";
+
+  if (props.spots === 0) {
+    spotNum = "no";
+  }
+
+  if (props.spots === 1) {
+    spotPlural = "spot";
+  }
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.number} spots remaining</h3>
+      <h3 className="text--light">{spotNum} {spotPlural} remaining</h3>
     </li>
   );
 }
