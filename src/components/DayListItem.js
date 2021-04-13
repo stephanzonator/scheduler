ffeff9c49c1fa11982b07c9cb00dea738c5fe3da
Stephanie <stephanzonator@hotmail.com>
2021-04-13 12:43:24 -0700

    status working for selected item

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,7 +5,7 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots
+    "day-list__item--full": (props.spots === 0)
   });
 
   let spotNum = props.spots;
@@ -20,7 +20,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => {props.setDay(props.name); console.log("set to ", props.name);}}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{spotNum} {spotPlural} remaining</h3>
     </li>
